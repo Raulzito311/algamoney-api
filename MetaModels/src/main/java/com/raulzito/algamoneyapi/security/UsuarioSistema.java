2b5d044d4b21c08a755153730982efac4cd804f0
Raulzito311 <raulmff@gmail.com>
2019-07-16 22:48:41 -0300

    Adicionando MetaModels

---
+++ MetaModels/src/main/java/com/raulzito/algamoneyapi/security/UsuarioSistema.java
@@ -0,0 +1,27 @@
+package com.raulzito.algamoneyapi.security;
+
+import com.raulzito.algamoneyapi.model.Usuario;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+
+import java.util.Collection;
+
+public class UsuarioSistema extends User {
+
+    private Usuario usuario;
+
+    public UsuarioSistema(Usuario usuario, Collection<? extends GrantedAuthority> authorities) {
+
+        super(usuario.getEmail(), usuario.getSenha(), authorities);
+        this.usuario = usuario;
+
+    }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
+}
